279be14a431cc4d2b73a62a0ea7473f6b2e0206b
miya0001 <miya@wpist.me>
2015-06-17 08:19:11 +0900

    some fix

--- provision-post.sh
+++ provision-post.sh
@@ -15,3 +15,12 @@ define('WP_DEBUG', true);
 define('WP_HOME', 'http://wckansai.dev');
 define('WP_SITEURL', 'http://wckansai.dev');
 PHP
+
+wp db reset --yes
+wp core install --url="http://wckansai.dev" --title="Welcome to the WordCamp Kansai 2015" --admin_user=admin --admin_password=admin --admin_email="admin@example.com"
+
+wp cron event run wp_update_plugins
+wp plugin update --all
+
+wp cron event run wp_update_themes
+wp theme update --all
