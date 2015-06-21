d278ed885fbb5ed4cb86fe4776512e4910d022e6
miya0001 <miya@wpist.me>
2015-06-21 15:27:32 +0900

    install language ja

--- provision-post.sh
+++ provision-post.sh
@@ -10,7 +10,7 @@ if [ -e /var/www/wordpress/wp-config.php ]; then
     rm -f /var/www/wordpress/wp-config.php
 fi
 
-# wp core download --path=/var/www/wordpress --locale=ja --force
+# wp core download --locale=ja --force
 
 wp core config --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --extra-php <<PHP
 define('WP_DEBUG', true);
@@ -21,6 +21,7 @@ PHP
 wp db reset --yes
 wp core install --url="http://wckansai.dev" --title="Welcome to the WordCamp Kansai 2015" \
     --admin_user=admin --admin_password=admin --admin_email="admin@example.com"
+wp core language activate ja
 
 # wp cron event run wp_update_plugins
 # wp plugin update --all
